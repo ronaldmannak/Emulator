72881332a1927caadbf221889d0210f2cf16e8e4
Ronald Mannak <ronaldmannak@me.com>
2019-11-20 21:08:03 -0800

    Initial Commit

---
+++ Emulator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Emulator
+//
+//  Created by Ronald "Danger" Mannak on 11/20/19.
+//  Copyright © 2019 A Puzzle A Day. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Emulator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Emulator
+//
+//  Created by Ronald "Danger" Mannak on 11/20/19.
+//  Copyright © 2019 A Puzzle A Day. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
